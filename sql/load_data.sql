1e47f631555e0976094765050d32bc73a856aaaa
Guilherme Oliveira <silva.guilhermeoliveira@gmail.com>
2024-03-19 23:58:14 -0300

    Initial commit

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -14,7 +14,7 @@ CREATE TABLE kaggle.machines (
 
 -- Criando a tabela 'errors'
 CREATE TABLE kaggle.erros (
-	error_id SMALLINT PRIMARY KEY,
+	error_id SERIAL PRIMARY KEY,
 	datetime TIMESTAMP NOT NULL,
 	machine_id SMALLINT REFERENCES kaggle.machines(machine_id) NOT NULL,
 	error_type VARCHAR(10) NOT NULL
@@ -22,7 +22,7 @@ CREATE TABLE kaggle.erros (
 
 -- Criando a tabela 'failures'
 CREATE TABLE kaggle.failures (
-	failure_id SMALLINT PRIMARY KEY,
+	failure_id SERIAL PRIMARY KEY,
 	datetime TIMESTAMP NOT NULL,
 	machine_id SMALLINT REFERENCES kaggle.machines(machine_id) NOT NULL,
 	failure_type VARCHAR(10) NOT NULL
@@ -30,7 +30,7 @@ CREATE TABLE kaggle.failures (
 
 -- Criando a tabela 'maint'
 CREATE TABLE kaggle.maint (
-	maint_id SMALLINT PRIMARY KEY,
+	maint_id SERIAL PRIMARY KEY,
 	datetime TIMESTAMP NOT NULL,
 	machine_id SMALLINT REFERENCES kaggle.machines(machine_id) NOT NULL,
 	component VARCHAR(10) NOT NULL 		
@@ -50,7 +50,7 @@ CREATE TABLE kaggle.telemetry (
 -- Copiando os os dados dos arquivos para as tabelas
 
 COPY kaggle.machines FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_machines.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.failures (datetime, machine_id, failure) FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_failures.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.maint (datetime, machine_id, component)FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_maint.csv' DELIMITER ',' CSV HEADER;
 COPY kaggle.erros (datetime, machine_id, error_type)FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_errors.csv' DELIMITER ',' CSV HEADER;
+COPY kaggle.failures (datetime, machine_id, failure_type) FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_failures.csv' DELIMITER ',' CSV HEADER;
+COPY kaggle.maint (datetime, machine_id, component)FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_maint.csv' DELIMITER ',' CSV HEADER;
 COPY kaggle.telemetry FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_telemetry.csv' DELIMITER ',' CSV HEADER;
