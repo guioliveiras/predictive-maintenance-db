afb3b29583c0de1e383c2b3286b5a4ffa7b3acc4
Guilherme Oliveira <silva.guilhermeoliveira@gmail.com>
2024-03-19 23:29:37 -0300

    aaa

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,52 +1,76 @@
--- Drop schema e criação
+<<<<<<<<<<<<<<  ✨ Codeium Command 🌟 >>>>>>>>>>>>>>>>
++-- Creating schemas and tables
+--- Drop schema e criação
+
 DROP SCHEMA IF EXISTS kaggle CASCADE;
 CREATE SCHEMA IF NOT EXISTS kaggle;
 
--- Criando a tabela "machines"
++-- Creating "machines" table
+--- Criando a tabela "machines"
 CREATE TABLE kaggle.machines (
-    machineID SMALLINT PRIMARY KEY,
++    machine_id SMALLINT PRIMARY KEY,
+-    machineID SMALLINT PRIMARY KEY,
     model VARCHAR(10) NOT NULL,
     age INT NOT NULL
 );
 
--- Criando a tabela "errors"
++-- Creating "errors" table
+--- Criando a tabela "errors"
 CREATE TABLE kaggle.errors (
-	error_id SMALLINT PRIMARY KEY,
-	datetime TIMESTAMP NOT NULL,
-	machineID SMALLINT REFERENCES kaggle.machines(machineID) NOT NULL,
-	errorID VARCHAR NOT NULL
++    error_id SMALLINT PRIMARY KEY,
++    datetime TIMESTAMP NOT NULL,
++    machine_id SMALLINT REFERENCES kaggle.machines(machine_id) NOT NULL,
++    error_id VARCHAR NOT NULL
+-	error_id SMALLINT PRIMARY KEY,
+-	datetime TIMESTAMP NOT NULL,
+-	machineID SMALLINT REFERENCES kaggle.machines(machineID) NOT NULL,
+-	errorID VARCHAR NOT NULL
 );
 
--- Criando a tabela "failures"
++-- Creating "failures" table
+--- Criando a tabela "failures"
 CREATE TABLE kaggle.failures (
     failure_id SERIAL PRIMARY KEY,
     datetime TIMESTAMP NOT NULL,
-    machineID INT REFERENCES kaggle.machines(machineID) NOT NULL,
++    machine_id INT REFERENCES kaggle.machines(machine_id) NOT NULL,
+-    machineID INT REFERENCES kaggle.machines(machineID) NOT NULL,
     failure VARCHAR(10) NOT NULL
 );
 
--- Criando a tabela "maint"
++-- Creating "maint" table
+--- Criando a tabela "maint"
 CREATE TABLE kaggle.maint (
     maint_id SERIAL PRIMARY KEY,
     datetime TIMESTAMP NOT NULL,
-    machineID INT REFERENCES kaggle.machines(machineID) NOT NULL,
++    machine_id INT REFERENCES kaggle.machines(machine_id) NOT NULL,
+-    machineID INT REFERENCES kaggle.machines(machineID) NOT NULL,
     component VARCHAR(10) NOT NULL
 );
 
--- Criando a tabela "telemetry"
++-- Creating "telemetry" table
+--- Criando a tabela "telemetry"
 CREATE TABLE kaggle.telemetry (
     datetime TIMESTAMP,
-    machineID INT REFERENCES kaggle.machines(machineID),
++    machine_id INT REFERENCES kaggle.machines(machine_id),
+-    machineID INT REFERENCES kaggle.machines(machineID),
     volt FLOAT NOT NULL,
     rotate FLOAT NOT NULL,
     pressure FLOAT NOT NULL,
     vibration FLOAT NOT NULL,
-    PRIMARY KEY (machineID, datetime)
++    PRIMARY KEY (machine_id, datetime)
+-    PRIMARY KEY (machineID, datetime)
 );
 
--- Copiando os dados dos arquivos para as tabelas.
-COPY kaggle.machines FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_machines.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.errors (datetime, machineID, errorID) FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_errors.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.failures (datetime, machineID, failure) FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_failures.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.maint (datetime, machineID, component) FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_maint.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.telemetry  FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_telemetry.csv' DELIMITER ',' CSV HEADER;
++-- Copying data from files to tables
++COPY kaggle.machines FROM 'data/PdM_machines.csv' DELIMITER ',' CSV HEADER;
++COPY kaggle.errors FROM 'data/PdM_errors.csv' DELIMITER ',' CSV HEADER;
++COPY kaggle.failures FROM 'data/PdM_failures.csv' DELIMITER ',' CSV HEADER;
++COPY kaggle.maint FROM 'data/PdM_maint.csv' DELIMITER ',' CSV HEADER;
++COPY kaggle.telemetry FROM 'data/PdM_telemetry.csv' DELIMITER ',' CSV HEADER;
+--- Copiando os dados dos arquivos para as tabelas.
+-COPY kaggle.machines FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_machines.csv' DELIMITER ',' CSV HEADER;
+-COPY kaggle.errors FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_errors.csv' DELIMITER ',' CSV HEADER;
+-COPY kaggle.failures FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_failures.csv' DELIMITER ',' CSV HEADER;
+-COPY kaggle.maint FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_maint.csv' DELIMITER ',' CSV HEADER;
+COPY kaggle.telemetry FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_telemetry.csv' DELIMITER ',' CSV HEADER;
+<<<<<<<  2cda61db-618a-4fbe-8185-0fb1d75b07cf  >>>>>>>
