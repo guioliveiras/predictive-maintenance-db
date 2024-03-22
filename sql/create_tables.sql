295532d023b4cd963141d9b454accb19e30d9f43
Guilherme Oliveira <silva.guilhermeoliveira@gmail.com>
2024-03-22 12:58:27 -0300

    Initial commit

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,118 +1,122 @@
--- Drop schema e criação
-DROP SCHEMA IF EXISTS maintenance_data CASCADE;
-CREATE SCHEMA maintenance_data;
+-- SCHEMA: maintenance
+
+DROP SCHEMA IF EXISTS maintenance CASCADE;
+
+CREATE SCHEMA IF NOT EXISTS maintenance
+    AUTHORIZATION postgres;
 
 -- Criação da tabela "components"
-CREATE TABLE maintenance_data.components (
-    component_key SMALLSERIAL PRIMARY KEY,
+CREATE TABLE maintenance.components (
+    component_key SERIAL PRIMARY KEY,
     component_name VARCHAR(10)
 );
 
 -- Inserção de dados na tabela "components"
-INSERT INTO maintenance_data.components (component_name)
+INSERT INTO maintenance.components (component_name)
 SELECT DISTINCT failure FROM kaggle.failures ORDER BY failure;
 
 -- Criação da tabela "failures"
-CREATE TABLE maintenance_data.failures AS (
+CREATE TABLE maintenance.failures AS (
     SELECT 
         failure_id,
         datetime,
         machineid,
         c.component_key
     FROM kaggle.failures f 
-    LEFT JOIN maintenance_data.components c ON f.failure = c.component_name
+    LEFT JOIN maintenance.components c ON f.failure = c.component_name
 );
 
 -- Definição de chaves primárias e estrangeiras para a tabela "failures"
-ALTER TABLE maintenance_data.failures 
+ALTER TABLE maintenance.failures 
     ADD CONSTRAINT pk_failure_id PRIMARY KEY(failure_id),
-    ADD CONSTRAINT fk_component_key FOREIGN KEY (component_key) REFERENCES maintenance_data.components(component_key);
+    ADD CONSTRAINT fk_component_key FOREIGN KEY (component_key) REFERENCES maintenance.components(component_key);
+
 
 -- Criação da tabela "maint"
-CREATE TABLE maintenance_data.maint AS (
+CREATE TABLE maintenance.maint AS (
     SELECT 
         maint_id,
         datetime,
         machineid AS machine_id,
         c.component_key 
     FROM kaggle.maint m 
-    LEFT JOIN maintenance_data.components c ON c.component_name = m.component 
+    LEFT JOIN maintenance.components c ON c.component_name = m.comp 
 );
 
 -- Definição de chaves primárias e estrangeiras para a tabela "maint"
-ALTER TABLE maintenance_data.maint 
+ALTER TABLE maintenance.maint 
     ADD CONSTRAINT pk_maint_id PRIMARY KEY(maint_id),
-    ADD CONSTRAINT fk_component_key FOREIGN KEY (component_key) REFERENCES maintenance_data.components(component_key);
+    ADD CONSTRAINT fk_component_key FOREIGN KEY (component_key) REFERENCES maintenance.components(component_key);
 
 -- Criação da tabela "error"
-CREATE TABLE maintenance_data.error (
+CREATE TABLE maintenance.error (
     error_key SERIAL PRIMARY KEY,
     error_id VARCHAR(10)
 );
 
 -- Inserção de dados na tabela "error"
-INSERT INTO maintenance_data.error (error_id)
+INSERT INTO maintenance.error (error_id)
 SELECT DISTINCT errorid FROM kaggle.errors ORDER BY errorid;
 
 -- Criação da tabela "error_machine"
-CREATE TABLE maintenance_data.error_machine AS (
+CREATE TABLE maintenance.error_machine AS (
     SELECT 
         em.error_id AS error_machine_id,
         em.datetime,
         em.machineid AS machine_id,
         e.error_key
     FROM kaggle.errors em 
-    LEFT JOIN maintenance_data.error e ON em.errorid  = e.error_id 
+    LEFT JOIN maintenance.error e ON em.errorid  = e.error_id 
 );
 
 -- Definição de chaves primárias e estrangeiras para a tabela "error_machine"
-ALTER TABLE maintenance_data.error_machine 
+ALTER TABLE maintenance.error_machine 
     ADD CONSTRAINT pk_error_machine_id PRIMARY KEY(error_machine_id),
-    ADD CONSTRAINT fk_error_key FOREIGN KEY (error_key) REFERENCES maintenance_data.error(error_key);
+    ADD CONSTRAINT fk_error_key FOREIGN KEY (error_key) REFERENCES maintenance.error(error_key);
 
 -- Criação da tabela "models"
-CREATE TABLE maintenance_data.models (
-    model_id SMALLSERIAL PRIMARY KEY,
+CREATE TABLE maintenance.models (
+    model_id SERIAL PRIMARY KEY,
     model VARCHAR(10) NOT NULL
 );
 
 -- Inserção de dados na tabela "models"
-INSERT INTO maintenance_data.models (model)
+INSERT INTO maintenance.models (model)
 SELECT DISTINCT model FROM kaggle.machines ORDER BY model;
 
 -- Criação da tabela "machine"
-CREATE TABLE maintenance_data.machine AS (
+CREATE TABLE maintenance.machine AS (
     SELECT 
         m.machineid AS machine_id,
         mds.model_id,
         2024 - m.age AS manufacture_year
     FROM kaggle.machines m 
-    LEFT JOIN maintenance_data.models mds ON mds.model = m.model
+    LEFT JOIN maintenance.models mds ON mds.model = m.model
 );
 
 -- Definição de chaves primárias e estrangeiras para a tabela "machine"
-ALTER TABLE maintenance_data.machine 
+ALTER TABLE maintenance.machine 
     ADD CONSTRAINT pk_machine_id PRIMARY KEY (machine_id),
-    ADD CONSTRAINT fk_model_id FOREIGN KEY (model_id) REFERENCES maintenance_data.models(model_id);
+    ADD CONSTRAINT fk_model_id FOREIGN KEY (model_id) REFERENCES maintenance.models(model_id);
 
 -- Definição de chaves estrangeiras para a tabela "maint", "failures", "error_machine"
-ALTER TABLE maintenance_data.maint ADD CONSTRAINT fk_machine_id FOREIGN KEY (machine_id) REFERENCES maintenance_data.machine(machine_id);
-ALTER TABLE maintenance_data.failures ADD CONSTRAINT fk_machine_id FOREIGN KEY (machineid) REFERENCES maintenance_data.machine(machine_id);
-ALTER TABLE maintenance_data.error_machine ADD CONSTRAINT fk_machine_id FOREIGN KEY (machine_id) REFERENCES maintenance_data.machine(machine_id);
+ALTER TABLE maintenance.maint ADD CONSTRAINT fk_machine_id FOREIGN KEY (machine_id) REFERENCES maintenance.machine(machine_id);
+ALTER TABLE maintenance.failures ADD CONSTRAINT fk_machine_id FOREIGN KEY (machine_id) REFERENCES maintenance.machine(machine_id);
+ALTER TABLE maintenance.error_machine ADD CONSTRAINT fk_machine_id FOREIGN KEY (machine_id) REFERENCES maintenance.machine(machine_id);
 
 -- Criação da tabela "telemetry"
-CREATE TABLE maintenance_data.telemetry (
+CREATE TABLE maintenance.telemetry (
     telemetry_id SERIAL PRIMARY KEY,
     machine_id SMALLINT NOT NULL,
     datetime TIMESTAMP NOT NULL,
-    volt DECIMAL(16, 13) NOT NULL,
-    rotate DECIMAL(16, 13) NOT NULL,
-    pressure DECIMAL(16, 13) NOT NULL,
-    vibration DECIMAL(15, 13) NOT NULL
+    volt NUMERIC(16, 13) NOT NULL,
+    rotate NUMERIC(16, 13) NOT NULL,
+    pressure NUMERIC(16, 13) NOT NULL,
+    vibration NUMERIC(15, 13) NOT NULL
 );
 
 -- Inserção de dados na tabela "telemetry"
-INSERT INTO maintenance_data.telemetry (machine_id, datetime, volt, rotate, pressure, vibration)
+INSERT INTO maintenance.telemetry (machine_id, datetime, volt, rotate, pressure, vibration)
 SELECT
     machineid,
     datetime,
@@ -123,11 +127,5 @@ SELECT
 FROM kaggle.telemetry;
 
 -- Definição de chave estrangeira para a tabela "telemetry"
-ALTER TABLE maintenance_data.telemetry 
-	ADD CONSTRAINT fk_machine_id FOREIGN KEY (machine_id) REFERENCES maintenance_data.machine(machine_id);
-
-
-
-
-
-
+ALTER TABLE maintenance.telemetry 
+	ADD CONSTRAINT fk_machine_id FOREIGN KEY (machine_id) REFERENCES maintenance.machine(machine_id);
--- sql/load_data.sql
+++ sql/load_data.sql
@@ -7,7 +7,7 @@ CREATE SCHEMA IF NOT EXISTS kaggle
 
 -- Criando a tabela 'machines'
 CREATE TABLE kaggle.machines (
-	machine_id SMALLINT PRIMARY KEY,
+	machineid SMALLINT PRIMARY KEY,
 	model VARCHAR(10) NOT NULL,
 	age INT NOT NULL
 );
@@ -16,41 +16,41 @@ CREATE TABLE kaggle.machines (
 CREATE TABLE kaggle.erros (
 	error_id SERIAL PRIMARY KEY,
 	datetime TIMESTAMP NOT NULL,
-	machine_id SMALLINT REFERENCES kaggle.machines(machine_id) NOT NULL,
-	error_type VARCHAR(10) NOT NULL
+	machineid SMALLINT REFERENCES kaggle.machines(machineid) NOT NULL,
+	errorid VARCHAR(10) NOT NULL
 );
 
 -- Criando a tabela 'failures'
 CREATE TABLE kaggle.failures (
 	failure_id SERIAL PRIMARY KEY,
 	datetime TIMESTAMP NOT NULL,
-	machine_id SMALLINT REFERENCES kaggle.machines(machine_id) NOT NULL,
-	failure_type VARCHAR(10) NOT NULL
+	machineid SMALLINT REFERENCES kaggle.machines(machineid) NOT NULL,
+	failure VARCHAR(10) NOT NULL
 );
 
 -- Criando a tabela 'maint'
 CREATE TABLE kaggle.maint (
 	maint_id SERIAL PRIMARY KEY,
 	datetime TIMESTAMP NOT NULL,
-	machine_id SMALLINT REFERENCES kaggle.machines(machine_id) NOT NULL,
-	component VARCHAR(10) NOT NULL 		
+	machineid SMALLINT REFERENCES kaggle.machines(machineid) NOT NULL,
+	comp VARCHAR(10) NOT NULL 		
 );
 
 -- Criando a tabela 'telemetry'
 CREATE TABLE kaggle.telemetry (
 	datetime TIMESTAMP NOT NULL,
-	machine_id SMALLINT REFERENCES kaggle.machines(machine_id) NOT NULL,
+	machineid SMALLINT REFERENCES kaggle.machines(machineid) NOT NULL,
 	volt FLOAT NOT NULL,
 	rotate FLOAT NOT NULL,
 	pressure FLOAT NOT NULL,
 	vibration FLOAT NOT NULL,
-	PRIMARY KEY(machine_id, datetime)
+	PRIMARY KEY(machineid, datetime)
 );
 
 -- Copiando os os dados dos arquivos para as tabelas
 
 COPY kaggle.machines FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_machines.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.erros (datetime, machine_id, error_type)FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_errors.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.failures (datetime, machine_id, failure_type) FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_failures.csv' DELIMITER ',' CSV HEADER;
-COPY kaggle.maint (datetime, machine_id, component)FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_maint.csv' DELIMITER ',' CSV HEADER;
+COPY kaggle.erros (datetime, machineid, errorid)FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_errors.csv' DELIMITER ',' CSV HEADER;
+COPY kaggle.failures (datetime, machineid, failure) FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_failures.csv' DELIMITER ',' CSV HEADER;
+COPY kaggle.maint (datetime, machineid, comp)FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_maint.csv' DELIMITER ',' CSV HEADER;
 COPY kaggle.telemetry FROM 'C:\Users\Guilherme\OneDrive\Documentos\projects\predictive-maintenance-db\data\PdM_telemetry.csv' DELIMITER ',' CSV HEADER;
